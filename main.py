3f36486467a5ab03a49525447cf90a98b9e507e4
INKSec <michael.burkhardt89@web.de>
2024-05-13 13:22:17 +0200

    added data files and modified main to read all three data files

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("test")
+import pandas as pd
+import pathlib
+
+
+# Get current path of main file
+mainPath = pathlib.Path(__file__).parent.resolve()
+# Path of data files
+weatherPath = '\\Daten\\wetter.csv'
+kiwoPath = '\\Daten\\kiwo.csv'
+umsatzdatenPath = '\\Daten\\umsatzdaten_gekuerzt.csv'
+
+# read weather data in dataframe
+myFrameWetter = pd.DataFrame()
+myFrameWetter = pd.read_csv(str(mainPath) + weatherPath)
+
+# read kiwo data in dataframe
+myFrameKiwo = pd.DataFrame()
+myFrameKiwo = pd.read_csv(str(mainPath) + kiwoPath)
+
+# read umsatz data in dataframe
+myFrameUmsatz = pd.DataFrame()
+myFrameUmsatz = pd.read_csv(str(mainPath) + umsatzdatenPath)
+
+# concat all three dataframes
+combinedDataframe = pd.concat([myFrameKiwo, myFrameUmsatz, myFrameWetter], axis=1)
+
+print(combinedDataframe)
+
+
